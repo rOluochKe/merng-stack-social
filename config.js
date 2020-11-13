b25f244d406479f7b47a347a9a21798012e8fc38
Raymond Oluoch <rasholuoch@gmail.com>
2020-11-13 13:37:43 +0300

    reconfiguration to database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://clusted_0:IqoxFWsRBrNpUJn1@cluster0-pcsru.mongodb.net/merng_stack_social?retryWrites=true',
-  SECRET_KEY: 'hasfdakuwejwe32bdwq'
+    'mongodb+srv://clusted_0:IqoxFWsRBrNpUJn1@cluster0.3aown.mongodb.net/merng_stack_social?retryWrites=true&w=majority',
+    SECRET_KEY: 'hasfdakuwejwe32bdwq'
 };
