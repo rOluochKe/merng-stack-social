f08e6937ed6ea945b6ca9e21f36e1ab3d2bd8469
Raymond Oluoch <rasholuoch@gmail.com>
2020-11-13 13:31:20 +0300

    project initialization and setup

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://clusted_0:IqoxFWsRBrNpUJn1@cluster0-pcsru.mongodb.net/merng_stack_social?retryWrites=true',
+  SECRET_KEY: 'hasfdakuwejwe32bdwq'
+};
